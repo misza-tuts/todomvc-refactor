b70b09404a3d307d9e4412c5ec428550ae723fdf
Katarzyna Frasunkiewicz <katarzyna.frasunkiewicz@atos.net>
2019-10-04 11:27:02 +0200

    Adds catch error and alert

--- js/restMod.js
+++ js/restMod.js
@@ -1,18 +1,35 @@
 import axios from 'axios'
-
+const alertInfo = 'Ups.. coś poszło nie tak';
 export const restMod = {
   async getTodos() {
-     const response = await axios.get('http://localhost:3000/todos');
-     return response.data;
+    try {
+      const response = await axios.get('http://localhost:3000/todos');
+      return response.data;
+    } catch (error) {
+      alert(alertInfo);
+      console.error(error);
+    }
   },
   async createTodo(todo) { 
-   await axios.post("http://localhost:3000/todos", todo);
-  },
-  async updateTodo(todo) { 
-     await axios.patch(`http://localhost:3000/todos/${todo.id}`, todo);
+    try {
+      await axios.post("http://localhost:3000/todos", todo);
+    } catch {
+      alert('Ups.. coś poszło nie tak');
+    }
 
   },
+  async updateTodo(todo) { 
+    try {
+      await axios.patch(`http://localhost:3000/todos/${todo.id}`, todo);
+    } catch (error) {
+    alert('Ups.. coś poszło nie tak');
+    }
+  },
   async deleteTodo(todo) {
-     await axios.delete(`http://localhost:3000/todos/${todo.id}`);
+    try {
+      await axios.delete(`http://localhost:3000/todos/${todo.id}`);
+    } catch (error) {
+      alert('Ups.. coś poszło nie tak');
+    }
    }
 };
